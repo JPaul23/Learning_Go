205a3f12ff15c666b721cb95146d3fcb7973f2a0
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-10-16 09:31:10 +0200

    json in go

---
+++ json-go/examples/text-to-interface/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	input := `{
+        "name": "John Doe",
+        "age": 15,
+        "hobbies": ["climbing", "cycling", "running"]
+    }`
+
+	var target map[string]any
+
+	err := json.Unmarshal([]byte(input), &target)
+	if err != nil {
+		log.Fatalf("Unable to marshal JSON due to %s", err)
+	}
+
+	for k, v := range target {
+		fmt.Printf("K: %s, V: %v\n", k, v)
+	}
+}

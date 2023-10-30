3fcc7506937783b5d9762f076ceb7ccae1a0aa8e
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-10-30 14:10:34 +0200

    Added zap as a logger

--- gin/main.go
+++ gin/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func v1EndpointHandler(ctx *gin.Context) {
@@ -12,11 +13,26 @@ func v2EndpointHandler(ctx *gin.Context) {
 	ctx.String(200, "v2: %s %s", ctx.Request.Method, ctx.Request.URL.Path)
 }
 
-func main() {
-	router := gin.Default()
+func logMiddleware() gin.HandlerFunc {
+	logger := zap.NewExample()
+	defer logger.Sync() // flushes buffer, if any
 
+	return func(ctx *gin.Context) {
+		logger.Info("incoming request",
+			zap.String("path", ctx.Request.URL.Path),
+			zap.String("method", ctx.Request.Method),
+		)
+		ctx.Next()
+	}
+}
+
+func main() {
+
+	router := gin.Default()
 	router.ForwardedByClientIP = true
 	router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.Use(logMiddleware())
 
 	v1 := router.Group("/v1")
 	v1.GET("/products", v1EndpointHandler)
